5985de9ebbd89dda6584972be0d9964b7353ccd9
ryuiro-kitsune <zane@beckerbyrd.com>
2021-09-07 20:05:08 -0700

    ranger config

--- scripts/link.sh
+++ scripts/link.sh
@@ -4,5 +4,5 @@ ln ryuiro-dotfiles/.xintrc ./
 ln ryuiro-dotfiles/.bashrc ./
 ln -s ryuiro-dotfiles/.i3 ./
 ln ryuiro-dotfiles/Xreasources ./
-ln -s ryuiro-dotfiles/ .config/
+ln -s ryuiro-dotfiles/ranger .config/
 ln .xinitrc ./
