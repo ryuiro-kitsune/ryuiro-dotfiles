c9bb9d595418251957d2ba31fce78e0ac6943717
ryuiro-kitsune <90160262+ryuiro-kitsune@users.noreply.github.com>
2021-09-10 18:02:46 -0700

    Update link.sh

--- scripts/link.sh
+++ scripts/link.sh
@@ -2,7 +2,7 @@
 
 ln ryuiro-dotfiles/.xintrc ./
 ln ryuiro-dotfiles/.bashrc ./
-ln -s ryuiro-dotfiles/.i3 ./
+ln -s ryuiro-dotfiles/i3 .config/
 ln ryuiro-dotfiles/.Xreasources ./
 ln -s ryuiro-dotfiles/ranger .config/
 ln .xinitrc ./
