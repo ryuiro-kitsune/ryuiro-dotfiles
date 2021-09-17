e51a87fdc174371bba5f268704ebad24d0beb556
ryuiro-kitsune <zane@beckerbyrd.com>
2021-09-17 13:26:43 -0700

    add vimrc

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-ln -fryuiro-dotfiles/.xintrc ./
-ln -f ryuiro-dotfiles/.bashrc ./
-ln -sf ryuiro-dotfiles/i3 .config/
-ln -f ryuiro-dotfiles/.Xreasources ./
-ln -sf ryuiro-dotfiles/ranger .config/
-ln -f .xinitrc ./
-ln -f ryuiro-dotfiles/ranger* ./.config/ranger/
-# ln ryuiro-dotfiles/.conky.conf ./
+ln -f ~/ryuiro-dotfiles/.xintrc ~/
+ln -f ~/ryuiro-dotfiles/.bashrc ~/
+mkdir ~/.config/i3
+ln -f ~/ryuiro-dotfiles/.i3 ~/.config/
+ln -f ~/ryuiro-dotfiles/.Xreasources ~/
+mkdir ~/.config/ranger
+ln -f ~/.xinitrc ~/
+ln -f ~/ryuiro-dotfiles/range/r* ~/.config/ranger/
+# ln ryuiro-dotfiles/.conky.conf ~/
+
+# Vim section
+
+mkdir ~/.vim
+mkdir ~/.vim/syntax
+cp ~/ryuiro-dotfiles/vim/syntax/* ~/.vim/syntax
+mkdir ~/.vim/plugins
+ln -f ~/ryuiro-dotfiles/vim/.vimrc
