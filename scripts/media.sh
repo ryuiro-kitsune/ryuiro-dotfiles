f5d6934d51405298a292611ce00167479d34dff1
ryuiro-kitsune <zane@beckerbyrd.com>
2021-10-19 19:50:45 -0700

    Added mpvacious as tmp fix until next month.

--- scripts/media.sh
+++ scripts/media.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 
 sudo pacman -S mpv
+mkdir -p $HOME/.config/mpv/scripts
+git clone https://github.com/Ajatt-Tools/mpvacious.git
 sudo pacman -S transmission-cli
 sudo pacman -S YouTube-dl
 sudo pacman -S graphicsmagick imagemagick
